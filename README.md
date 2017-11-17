462ae9c862523a2b921ead1f4064b078f30454b8
onvno <leewei2020@126.com>
2017-11-17 21:28:41 +0800

    0.1.1版本：增加type & node 参数

--- README.md
+++ README.md
@@ -19,10 +19,15 @@ $ npm install -g ncbuild
 # 进入项目目录
 $ cd project
 
-# 项目目录创建模板,本例模板名为sessions
-$ ncbuild --out sessions
+# 项目目录创建模板
+$ ncbuild --out Name --type TypeName --node NodeName
 或
-$ ncbuild -O sessions
+$ ncbuild -O Name -T TypeName -N NodeName
+
+# 以training节点为例，单据类型PT32,菜单编码PTH10302
+$ ncbuild --out training --type PT32 --node PTH10302
 ```
 
-通过以上操作，在`project`目录快速创建`sessions`节点模板。
+支持参数为小写，如`PT32`,输入为`pt32`
+
+通过以上操作，在`project`目录快速创建`training`节点模板。
--- index.js
+++ index.js
@@ -10,11 +10,46 @@ const os = require('os');
 // __dirname - 原始路径
 // process.cwd() - 运行路径
 
+
+
 const argv = require('yargs')
+.demand(['out','type','node'])
 .option('out', {
   alias: 'O',
-  describe: '当前目录输出',
+  describe: '当前目录输出模板名称',
+})
+.option('type', {
+  alias: 'T',
+  describe: '模板单据类型billtype名称',
+})
+.option('node', {
+  alias: 'N',
+  describe: '模板节点funcnode名称',
 }).help().argv;
+
+const REPLACE = {
+  name: /caugment/g,
+  upperName: /Caugment/g,
+  type: /PT26/g,
+  node: /PTH10206/g,
+}
+
+const lowerOutName = argv.out.toLowerCase();
+const upperOutName = UpperFirstLetter(lowerOutName);
+const upperOutType = argv.type.toUpperCase();
+const upperOutNode = argv.node.toUpperCase();
+function UpperFirstLetter(str) {
+  const lowerAry = str.split("");
+  lowerAry[0] = String.fromCharCode(lowerAry[0].charCodeAt() - 32);
+  return lowerAry.join("");
+}
+
+const OUT = {
+  name: lowerOutName,
+  upperName: upperOutName,
+  type: upperOutType,
+  node: upperOutNode,
+}
 
 const baseDir = path.join(__dirname, 'caugment');
 const destDir = path.join(process.cwd(),argv.out);
@@ -22,10 +57,8 @@ const destDir = path.join(process.cwd(),argv.out);
 const pattern = path.join(baseDir, '**/*');
 const enters = glob.sync(pattern);
 
-
-
 enters.forEach(function(item, index){
-  const dest = item.replace(/caugment/g, argv.out).replace(__dirname, process.cwd());
+  const dest = item.replace(REPLACE.name, argv.out).replace(__dirname, process.cwd());
   
   const isDir = fs.statSync(item).isDirectory();
 
@@ -41,7 +74,12 @@ enters.forEach(function(item, index){
     });
     
     rl.on('line', (line) => {
-      var newLine = line.replace(/caugment/g, argv.out) + os.EOL;
+      var newLine = line
+        .replace(REPLACE.name, OUT.name)
+        .replace(REPLACE.type, OUT.type)
+        .replace(REPLACE.node, OUT.node)
+        .replace(REPLACE.upperName, OUT.upperName)
+        + os.EOL;
       fWrite.write(newLine);
     });
 
